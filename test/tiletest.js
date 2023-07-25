7159d199d2ee5bebe63fb30448d1eacd1388cd2d
martin jakubik <martin.jakubik@gmail.com>
2023-07-25 19:31:16 +0200

    added ismiddle flag

--- test/tiletest.js
+++ test/tiletest.js
@@ -5,69 +5,82 @@ const tiles2x2 = [{
     x: 0,
     y: 0,
     width: tileWidths[0],
-    height: tileHeights[0]
+    height: tileHeights[0],
+    isMiddle: false
 }, {
     x: tileWidths[0],
     y: 0,
     width: tileWidths[1],
-    height: tileHeights[0]
+    height: tileHeights[0],
+    isMiddle: false
 }, {
     x: 0,
     y: tileHeights[0],
     width: tileWidths[0],
-    height: tileHeights[1]
+    height: tileHeights[1],
+    isMiddle: false
 }, {
     x: tileWidths[0],
     y: tileHeights[0],
     width: tileWidths[1],
-    height: tileHeights[1]
+    height: tileHeights[1],
+    isMiddle: false
 }];
 
 const tiles3x3 = [{
     x: 0,
     y: 0,
     width: tileWidths[0],
-    height: tileHeights[0]
+    height: tileHeights[0],
+    isMiddle: false
 }, {
     x: tileWidths[0],
     y: 0,
     width: tileWidths[1],
-    height: tileHeights[0]
+    height: tileHeights[0],
+    isMiddle: false
 }, {
     x: tileWidths[0] + tileWidths[1],
     y: 0,
     width: tileWidths[2],
-    height: tileHeights[0]
+    height: tileHeights[0],
+    isMiddle: false
 }, {
     x: 0,
     y: tileHeights[0],
     width: tileWidths[0],
-    height: tileHeights[1]
+    height: tileHeights[1],
+    isMiddle: false
 }, {
     x: tileWidths[0],
     y: tileHeights[0],
     width: tileWidths[1],
-    height: tileHeights[1]
+    height: tileHeights[1],
+    isMiddle: true
 }, {
     x: tileWidths[0] + tileWidths[1],
     y: tileHeights[0],
     width: tileWidths[2],
-    height: tileHeights[1]
+    height: tileHeights[1],
+    isMiddle: false
 }, {
     x: 0,
     y: tileHeights[0] + tileHeights[1],
     width: tileWidths[0],
-    height: tileHeights[2]
+    height: tileHeights[2],
+    isMiddle: false
 }, {
     x: tileWidths[0],
     y: tileHeights[0] + tileHeights[1],
     width: tileWidths[1],
-    height: tileHeights[2]
+    height: tileHeights[2],
+    isMiddle: false
 }, {
     x: tileWidths[0] + tileWidths[1],
     y: tileHeights[0] + tileHeights[1],
     width: tileWidths[2],
-    height: tileHeights[2]
+    height: tileHeights[2],
+    isMiddle: false
 }];
 
 export { tiles2x2, tiles3x3 };
