1f9ca6e41a203e23559403a7ed533902292f4720
martin jakubik <martin.jakubik@sap.com>
2025-04-15 14:48:03 +0200

    reversed palette: light mode is default

--- app/palette.mjs
+++ app/palette.mjs
@@ -1,19 +1,6 @@
 const palette = [];
 
 palette[0] = {
-    lightMode: false,
-    bgColors: [
-        '#000000',
-        '#000000'
-    ],
-    fgColors: [
-        '#ffffff',
-        'rgba(250, 240, 240, 0.3)',
-        '#aaaaaa'
-    ]
-};
-
-palette[1] = {
     lightMode: true,
     bgColors: [
         '#ffffff',
@@ -22,6 +9,19 @@ palette[1] = {
     fgColors: [
         '#ffffff',
         'rgba(50, 40, 40, 0.3)',
+        '#aaaaaa'
+    ]
+};
+
+palette[1] = {
+    lightMode: false,
+    bgColors: [
+        '#000000',
+        '#000000'
+    ],
+    fgColors: [
+        '#ffffff',
+        'rgba(250, 240, 240, 0.3)',
         '#aaaaaa'
     ]
 };
